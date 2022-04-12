6be90178c780565017417eee22ff19364c48803d
dariush <dariushtaghavi9797@gmail.com>
2022-04-12 14:45:33 +0430

    add context to index screen

--- src/features/screens/IndexScreen.js
+++ src/features/screens/IndexScreen.js
@@ -1,8 +1,17 @@
-import React from 'react';
-import {Text} from 'react-native';
+import React, { useContext } from 'react';
+import {View, Text, FlatList} from 'react-native';
+import {BlogContext} from '../../services/blog-context.js/BlogContext';
 
 export const IndexScreen = () => {
+    const {Blogs} = useContext(BlogContext);
     return (
+        <View>
         <Text>index screen</Text>
+        <FlatList 
+        data = {Blogs}
+        keyExtractor = {(key) => {key.title} }
+        renderItem = { ({item}) => <Text>{item.title}</Text>}
+        />
+        </View>
     );
 }
