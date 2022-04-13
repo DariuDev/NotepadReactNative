b08acbd364fe18ce9384d12476ebe682a0e2bd62
dariush <dariushtaghavi9797@gmail.com>
2022-04-13 10:40:06 +0430

    create add post function

--- src/services/blog-context.js/BlogContext.js
+++ src/services/blog-context.js/BlogContext.js
@@ -1,11 +1,15 @@
-import React, {createContext} from 'react';
+import React, {createContext, useState} from 'react';
 
 export const BlogContext = createContext();
 
 export const BlogProvider = ({children}) => {
-    const Blogs = [{title:'#1'} , {title:'#2'}]
+    const [BlogPost, setBlogPost] = useState([]);
+
+    const add = () => {
+        setBlogPost([...BlogPost, {title : `Blog Post #${BlogPost.length + 1}`}])
+    };
     return (
-        <BlogContext.Provider value={{Blogs}}>
+        <BlogContext.Provider value={{BlogPost, addBlogPost : add}}>
             {children}
         </BlogContext.Provider>
     );
