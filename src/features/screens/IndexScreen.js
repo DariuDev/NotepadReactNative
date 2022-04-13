6a066e84c90946081d2575642a7ab6b798420d9c
dariush <dariushtaghavi9797@gmail.com>
2022-04-13 10:40:34 +0430

    move data from context to screen

--- src/features/screens/IndexScreen.js
+++ src/features/screens/IndexScreen.js
@@ -1,14 +1,15 @@
 import React, { useContext } from 'react';
-import {View, Text, FlatList} from 'react-native';
+import {View, Text, FlatList, Button} from 'react-native';
 import {BlogContext} from '../../services/blog-context.js/BlogContext';
 
 export const IndexScreen = () => {
-    const {Blogs} = useContext(BlogContext);
+    const {BlogPost, addBlogPost} = useContext(BlogContext);
     return (
         <View>
+            <Button title={'ADD'} onPress={addBlogPost}/>
         <Text>index screen</Text>
         <FlatList 
-        data = {Blogs}
+        data = {BlogPost}
         keyExtractor = {(key) => {key.title} }
         renderItem = { ({item}) => <Text>{item.title}</Text>}
         />
