03ffb2e4165b5841cf2b2ba4dcae53c5acc5e679
dariush <dariushtaghavi9797@gmail.com>
2022-04-20 10:49:09 +0430

    list styling

--- src/features/screens/IndexScreen.js
+++ src/features/screens/IndexScreen.js
@@ -1,18 +1,37 @@
-import React, { useContext } from 'react';
-import {View, Text, FlatList, Button} from 'react-native';
+import React, {useContext} from 'react';
+import {View, Text, FlatList, Button, StyleSheet, TouchableOpacity} from 'react-native';
 import {Context} from '../../services/context/blogContext/BlogContext';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 export const IndexScreen = () => {
-    const {state, addBlogPost} = useContext(Context);
-   return (
-        <View>
-            <Button title={'ADD'} onPress={addBlogPost}/>
-        <Text>index screen</Text>
-        <FlatList 
-        data = {state}
-        keyExtractor = {(key) => {key.title} }
-        renderItem = { ({item}) => <Text>{item.title}</Text>}
-        />
-        </View>
-    );
-}
+  const {state, addBlogPost} = useContext(Context);
+
+  return (
+    <View>
+      <Button title={'ADD'} onPress={addBlogPost} />
+      <FlatList
+        data={state}
+        keyExtractor={key => {
+          key.title;
+        }}
+        renderItem={({item}) =>(
+           <View style={styles.items}>
+          <Text>{item.title}</Text>
+          <TouchableOpacity>
+          <Ionicons name={"md-restaurant"} size={20}></Ionicons>
+          </TouchableOpacity>
+          </View> 
+          )} 
+      />
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+items : {
+  padding: 10,
+  borderTopWidth: 1,
+  borderTopColor: 'black',
+  flexDirection: 'row',
+  justifyContent: 'space-between'
+},
+});
