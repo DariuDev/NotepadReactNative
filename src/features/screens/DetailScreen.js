b7ddc88719a018c63b910a6ab63fa5bcb6e74c2c
dariush <dariushtaghavi9797@gmail.com>
2022-04-20 16:04:54 +0430

    pass data from index to detail

---
+++ src/features/screens/DetailScreen.js
@@ -0,0 +1,13 @@
+import React, {useContext} from 'react';
+import {Text, View} from 'react-native';
+import {Context} from '../../services/context/blogContext/BlogContext';
+
+export const DetailScreen = ({route}) => {
+  const {state} = useContext(Context);
+  console.log(route.params.id)
+
+  const blogPost = state.find(
+    blogPost => blogPost.id === route.params.id
+  );
+  return <View><Text>{blogPost.title}</Text></View>;
+};
--- src/features/screens/IndexScreen.js
+++ src/features/screens/IndexScreen.js
@@ -1,9 +1,16 @@
 import React, {useContext} from 'react';
-import {View, Text, FlatList, Button, StyleSheet, TouchableOpacity} from 'react-native';
+import {
+  View,
+  Text,
+  FlatList,
+  Button,
+  StyleSheet,
+  TouchableOpacity,
+} from 'react-native';
 import {Context} from '../../services/context/blogContext/BlogContext';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
-export const IndexScreen = () => {
+export const IndexScreen = ({navigation}) => {
   const {state, addBlogPost, deleteBlogPost} = useContext(Context);
 
   return (
@@ -14,24 +21,28 @@ export const IndexScreen = () => {
         keyExtractor={key => {
           key.title;
         }}
-        renderItem={({item}) =>(
-           <View style={styles.items}>
-          <Text>{item.title}-{item.id}</Text>
-          <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
-          <Ionicons name={"md-restaurant"} size={20}></Ionicons>
+        renderItem={({item}) => (
+          <TouchableOpacity
+            onPress={() => navigation.navigate('Detail', {id: item.id})}
+            style={styles.items}>
+            <Text>
+              {item.title}-{item.id}
+            </Text>
+            <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
+              <Ionicons name={'md-restaurant'} size={20}></Ionicons>
+            </TouchableOpacity>
           </TouchableOpacity>
-          </View> 
-          )} 
+        )}
       />
     </View>
   );
 };
 const styles = StyleSheet.create({
-items : {
-  padding: 10,
-  borderTopWidth: 1,
-  borderTopColor: 'black',
-  flexDirection: 'row',
-  justifyContent: 'space-between'
-},
+  items: {
+    padding: 10,
+    borderTopWidth: 1,
+    borderTopColor: 'black',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
 });
