dd92e2fad9d8065118b3951b133d55992c8334c2
dariush <dariushtaghavi9797@gmail.com>
2022-04-21 12:00:00 +0430

    add text Input and button in create screen

--- src/features/screens/CreateNoteScreen.js
+++ src/features/screens/CreateNoteScreen.js
@@ -1,8 +1,35 @@
-import React from 'react';
-import {View,Text} from 'react-native';
+import React, {useState} from 'react';
+import {View} from 'react-native';
+import {TextInput, Button} from 'react-native-paper';
 
 export const CreateNoteScreen = () => {
-    return (
-        <View><Text>create note</Text></View>
-    );
-}
+  const [title, setTitle] = useState();
+  const [content, setContent] = useState();
+  return (
+    <View>
+      <TextInput
+        label="Enter Title :"
+        textContentType="Title"
+        keyboardType="Enter A Title"
+        autoCapitalize="none"
+        value={title}
+        onChangeText={text => setTitle(text)}
+        style={{margin: 10}}
+      />
+      <TextInput
+        label="Enter Content :"
+        textContentType="Title"
+        keyboardType="Enter A Title"
+        autoCapitalize="none"
+        value={content}
+        onChangeText={text => setContent(text)}
+        style={{margin: 10}}
+      />
+      <Button
+        style={{marginLeft: 40, marginRight: 40, marginTop: 10}}
+        mode="outlined">
+        Add Note
+      </Button>
+    </View>
+  );
+};
