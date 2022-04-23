a697f61b120ea2f36e2144c97f2e68eb6b70366a
dariush <dariushtaghavi9797@gmail.com>
2022-04-23 13:31:15 +0430

    add inputs in edit screen

--- src/features/screens/EditScreen.js
+++ src/features/screens/EditScreen.js
@@ -1,10 +1,45 @@
-import React from 'react';
+import React,{useContext, useState} from 'react';
 import {View, Text} from 'react-native';
+import {TextInput, Button} from 'react-native-paper';
+import {Context} from '../../services/context/blogContext/BlogContext';
 
 export const EditScreen = ({route}) => {
-    return (
-        <View>
-            <Text>Edit Screen-{route.params.id}</Text>
-        </View>
-    );
-}
+    const {state} = useContext(Context);
+  const blogPost = state.find(blogPost => blogPost.id === route.params.id);
+  const [title, setTitle] = useState(blogPost.title);
+  const [content, setContent] = useState(blogPost.content);
+  return (
+    <View>
+      <View>
+        <TextInput
+          label="Enter Title :"
+          textContentType="Title"
+          keyboardType="Enter A Title"
+          autoCapitalize="none"
+          value={title}
+          onChangeText={newText => setTitle(newText)}
+          style={{margin: 10}}
+        />
+        <TextInput
+          label="Enter Content :"
+          textContentType="Title"
+          keyboardType="Enter A Title"
+          autoCapitalize="none"
+          value={content}
+          onChangeText={newText => setContent(newText)}
+          style={{margin: 10}}
+        />
+        <Button
+          style={{marginLeft: 40, marginRight: 40, marginTop: 10}}
+          mode="outlined"
+          onPress={() =>
+            addBlogPost(title, content, () => {
+              navigation.navigate('Notepad');
+            })
+          }>
+          Save
+        </Button>
+      </View>
+    </View>
+  );
+};
