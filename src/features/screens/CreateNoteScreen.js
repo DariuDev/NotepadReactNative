9abfc8c2953b9246dd9c3e962d57972519eb69a0
dariush <dariushtaghavi9797@gmail.com>
2022-04-24 11:29:22 +0430

    add note input

--- src/features/screens/CreateNoteScreen.js
+++ src/features/screens/CreateNoteScreen.js
@@ -1,42 +1,13 @@
-import React, {useContext, useState} from 'react';
-import {View} from 'react-native';
-import {TextInput, Button} from 'react-native-paper';
+import React, {useContext} from 'react';
 import {Context} from '../../services/context/blogContext/BlogContext';
+import NoteInput from '../../components/NoteInput';
 
 export const CreateNoteScreen = ({navigation}) => {
-  const [title, setTitle] = useState();
-  const [content, setContent] = useState();
   const {addBlogPost} = useContext(Context);
 
   return (
-    <View>
-      <TextInput
-        label="Enter Title :"
-        textContentType="Title"
-        keyboardType="Enter A Title"
-        autoCapitalize="none"
-        value={title}
-        onChangeText={text => setTitle(text)}
-        style={{margin: 10}}
-      />
-      <TextInput
-        label="Enter Content :"
-        textContentType="Title"
-        keyboardType="Enter A Title"
-        autoCapitalize="none"
-        value={content}
-        onChangeText={text => setContent(text)}
-        style={{margin: 10}}
-      />
-      <Button
-        style={{marginLeft: 40, marginRight: 40, marginTop: 10}}
-        mode="outlined"
-        onPress={
-          () => addBlogPost(title, content, () => {
-            navigation.navigate('Notepad');
-          }) }>
-        Add Note
-      </Button>
-    </View>
+   <NoteInput onSubmit={(title, content)=> {
+     addBlogPost(title, content, () => navigation.navigate('Notepad')) 
+   }}/>
   );
 };
--- src/features/screens/EditScreen.js
+++ src/features/screens/EditScreen.js
@@ -1,45 +1,19 @@
-import React,{useContext, useState} from 'react';
-import {View, Text} from 'react-native';
-import {TextInput, Button} from 'react-native-paper';
-import {Context} from '../../services/context/blogContext/BlogContext';
+import React, {useContext} from 'react';
 
-export const EditScreen = ({route}) => {
-    const {state} = useContext(Context);
-  const blogPost = state.find(blogPost => blogPost.id === route.params.id);
-  const [title, setTitle] = useState(blogPost.title);
-  const [content, setContent] = useState(blogPost.content);
+import {Context} from '../../services/context/blogContext/BlogContext';
+import NoteInput from '../../components/NoteInput';
+
+export const EditScreen = ({route, navigation}) => {
+  const id = route.params.id
+  const {state, editBlogPost} = useContext(Context);
+  const blogPost = state.find(blogPost => blogPost.id === id);
+
   return (
-    <View>
-      <View>
-        <TextInput
-          label="Enter Title :"
-          textContentType="Title"
-          keyboardType="Enter A Title"
-          autoCapitalize="none"
-          value={title}
-          onChangeText={newText => setTitle(newText)}
-          style={{margin: 10}}
-        />
-        <TextInput
-          label="Enter Content :"
-          textContentType="Title"
-          keyboardType="Enter A Title"
-          autoCapitalize="none"
-          value={content}
-          onChangeText={newText => setContent(newText)}
-          style={{margin: 10}}
-        />
-        <Button
-          style={{marginLeft: 40, marginRight: 40, marginTop: 10}}
-          mode="outlined"
-          onPress={() =>
-            addBlogPost(title, content, () => {
-              navigation.navigate('Notepad');
-            })
-          }>
-          Save
-        </Button>
-      </View>
-    </View>
+    <NoteInput
+      initialValue={{title: blogPost.title, content: blogPost.content}}
+      onSubmit={(title, content) => {
+        editBlogPost(id, title, content, () => navigation.pop())
+      }}
+    />
   );
 };
